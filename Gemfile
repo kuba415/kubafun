979bb57f01a26aefa9a7a3bdacef1864b5b51080
Jakub Glodek <jakub.glodek@gmail.com>
2018-01-11 14:52:07 -0800

    Add image upload with Paperclip

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass'
 gem 'jquery-rails'
 gem 'devise', '~> 4.4.0'
+gem "paperclip", "~> 5.1.0"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Required by Paperclip for Dev
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
