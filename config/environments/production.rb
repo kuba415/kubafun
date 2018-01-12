3c4773437f65c44f76b9374cccaeb82fa624dbcb
Jakub Glodek <jakub.glodek@gmail.com>
2018-01-12 11:27:07 -0800

    fix more shit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,9 +95,6 @@ Rails.application.configure do
   # Sets S3 location for image pins.
   config.paperclip_defaults = {
     storage: :s3,
-    url: :s3_domain_url,
-    path: '/:class/:attachment/:id_partition/:style/:filename',
-    s3_host_name: 's3-us-west-2.amazonaws.com',
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
